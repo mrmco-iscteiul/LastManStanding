1902a619ab06d8114c536ac49020b92d551fa03c
Michael Carvalho <mrmco@iscte-iul.pt>
2017-10-15 09:28:26 +0100

    Preparing UI

--- src/main/java/pt/iscte/pi/App.java
+++ src/main/java/pt/iscte/pi/App.java
@@ -1,7 +1,9 @@
 package pt.iscte.pi;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+	
+    public static void main(String[] args) {
+    		System.out.println("Creation of the User Interface");
     }
+    
 }
